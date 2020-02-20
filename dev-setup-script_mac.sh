cbd7b5cbf79323042bd1f72c9daf5c6914c55d48
Utkarsh <utkarsh@MacBook-Pro.local>
2020-02-21 00:28:28 +0530

    cowsay

--- dev-setup-script_mac.sh
+++ dev-setup-script_mac.sh
@@ -14,20 +14,22 @@
 
 
 
-if [ `echo $SHELL == "/bin/zsh"` ];
+if [ `echo $SHELL == "/bin/zsh"` -o `echo $SHELL == "/usr/local/bin/zsh"` ];
 	then
+	echo "installing cowsay. Trust me"
+	brew install cowsay
 	read -p "Do you want to install curl ( Required for further steps ) [y/n]: "  choice
 	y="y"
 	n="n"
 	if [ $choice == $y ]
 	then
-		echo "installing curl"
+		cowsay -s installing curl
 		brew install curl
 	else
 		break
-		echo "I know I know"
+		cowsay -t I know I know
 	fi
-	echo "installing ohmyzsh"
+	cowsay -t installing ohmyzsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	# curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 	sh install.sh
@@ -38,12 +40,12 @@ if [ `echo $SHELL == "/bin/zsh"` ];
 	n="n"
 	if [ $choice == $y ]
 	then
-	echo "Installing node"
+	cowsay -w "Installing node"
 	brew install node
 	node -v
 	else
 		break
-		echo "I know I know"
+		cowsay -t "I know I know"
 	fi
 
 	read -p "Do you want to install git[y/n]: "  choice
@@ -52,78 +54,77 @@ if [ `echo $SHELL == "/bin/zsh"` ];
 	n="n"
 	if [ $choice == $y ]
 	then
-	echo "Installing git"
+	cowsay -y "Installing git"
 	brew install git
 	else
 		break
-		echo "I know I know"
+		cowsay -t "I know I know"
 	fi
 
 
 	read -p "Do you want to install sdkman[y/n]: "  choice
 	#read -p "Enter your name : " name
-	#echo "Hi, $name. Let us be friends!"
+	#cowsay - "Hi, $name. Let us be friends!"
 	y="y"
 	n="n"
 	if [ $choice == $y ]
 	then
-	echo "Installing sdkman"
+	cowsay -b "Installing sdkman"
 	curl -s "https://get.sdkman.io" | bash
 	source "$HOME/.sdkman/bin/sdkman-init.sh"
 	sdk version
 	else
 		break
-		echo "I know I know"
+		cowsay -t "I know I know"
 	fi
 
 	read -p "Do you want to install visual studio code ( note: This will install cask, which is ~200mbs, but is required for further steps )[y/n]: "  choice
 	#read -p "Enter your name : " name
-	#echo "Hi, $name. Let us be friends!"
+	#cowsay - "Hi, $name. Let us be friends!"
 	y="y"
 	n="n"
 	if [ $choice == $y ]
 	then
-	echo "Installing vscode"
+	cowsay -g "Installing vscode"
 	brew tap caskroom/cask
 	brew cask install visual-studio-code 
 	brew cleanup
 	else
 		break
-		echo "I know I know"
+		cowsay -t "I know I know"
 	fi
 
 	read -p "Do you want to install postman[y/n]: "  choice
 	#read -p "Enter your name : " name
-	#echo "Hi, $name. Let us be friends!"
+	#cowsay - "Hi, $name. Let us be friends!"
 	y="y"
 	n="n"
 	if [ $choice == $y ]
 	then
-	echo "Installing postman"
+	cowsay -d "Installing postman"
 	brew cask install postman
 	else
 		break
-		echo "I know I know"
+		cowsay -t "I know I know"
 	fi
 
 	read -p "Do you want to install intellij-idea-community[y/n]: "  choice
 	#read -p "Enter your name : " name
-	#echo "Hi, $name. Let us be friends!"
+	#cowsay - "Hi, $name. Let us be friends!"
 	y="y"
 	n="n"
 	if [ $choice == $y ]
 	then
-	echo "Installing intellij"
+	cowsay -g "Installing intellij"
 	brew cask install intellij-idea-ce
 	else
 		break
-		echo "I know I know"
+		cowsay -t "I know I know"
 	fi
 	
 
 
 else
-
 	read -p "This script requires homebrew to run. Do you have homebrew installed? [y/n]: "  choice
 	y="y"
 	n="n"
@@ -135,10 +136,12 @@ else
 		exit
 	fi
 
-	echo "installing zsh"
+	echo "installing cowsay. Trust me"
+	brew install cowsay
+	cowsay -b installing zsh
 	brew install zsh
 	command -v zsh | sudo tee -a /etc/shells
 	sudo chsh -s "$(command -v zsh)" "${USER}"
-	echo $SHELL
-	echo "Logout and log back in to make zsh your default shell. This step is required for further steps. Run the script again after loggin back in"
+	cowsay -d $SHELL
+	cowsay -p Logout and log back in to make zsh your default shell. This step is required for further steps. Run the script again after loggin back in
 fi
